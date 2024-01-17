cc97a2c2dd80d4c090ec8eed67e0d03c6529ec14
Amit Bilapatte <agbilapatte78@gmail.com>
2024-01-17 17:47:38 +0530

    change code with functional approach

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("JAI SHREE RAM");
-// });
-
-// app.listen(3000);
-
 const fs = require("fs");
 const fileName = "target.txt";
 
-const data = fs.readFileSync(fileName);
-console.log(data.toString());
+const errHanlder = (err) => console.log(err);
 
-// fs.watch(fileName, () => console.log("file changed"));
-// fs.readFile(fileName, (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString());
-// });
+const dataHanlder = (data) => console.log(data.toString());
+fs.readFile(fileName, (err, data) => {
+  if (err) errHanlder(err);
+  dataHanlder(data);
+});
 console.log("node js async programming");
