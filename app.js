1dd7da49715f1985c9f8262178e9d0d0c63b86ad
Amit Bilapatte <agbilapatte78@gmail.com>
2024-01-17 17:17:11 +0530

    node async example

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.send("JAI SHREE RAM");
+// app.get("/", (req, res) => {
+//   res.send("JAI SHREE RAM");
+// });
+
+// app.listen(3000);
+
+const fs = require("fs");
+const fileName = "target.txt";
+
+// fs.watch(fileName, () => console.log("file changed"));
+fs.readFile(fileName, (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
 });
-
-app.listen(3000);
+console.log("node js async programming");
