e1be93fb3fb7fc11a29a7719127d25cf6b7f62e1
Amit Bilapatte <agbilapatte78@gmail.com>
2024-01-17 17:20:45 +0530

    synchronous progamming in nodejs

--- app.js
+++ app.js
@@ -10,11 +10,14 @@
 const fs = require("fs");
 const fileName = "target.txt";
 
+const data = fs.readFileSync(fileName);
+console.log(data.toString());
+
 // fs.watch(fileName, () => console.log("file changed"));
-fs.readFile(fileName, (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
-});
+// fs.readFile(fileName, (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
 console.log("node js async programming");
