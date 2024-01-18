30caed9b98306edd4856be7d4133cf4c9b249475
Amit Bilapatte <agbilapatte78@gmail.com>
2024-01-18 13:58:04 +0530

    use bodyParser; add models connecting to database schema; add post request

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,28 @@
+const Post = require("../models/post");
+
 exports.getPosts = (req, res) => {
   res.json({
     posts: [{ title: "JAI" }, { title: "SHREE" }, { title: "RAM" }],
   });
+};
+
+// exports.createPost = (req, res) => {
+//   const post = new Post(req.body);
+//   // console.log("Creating Post : ", req.body);
+//   post.save((err, result) => {
+//     if (err) {
+//       return res.status();
+//     }
+//   });
+// };
+
+exports.createPost = async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    // console.log("Creating Post : ", req.body);
+    const result = await post.save();
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
