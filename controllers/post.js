5a0be9b7a90d9517c3b312ab3cb46a1f22ab40dd
Amit Bilapatte <agbilapatte78@gmail.com>
2024-01-18 15:11:40 +0530

    Update getPostd method to retrieve only required parameters from db

--- controllers/post.js
+++ controllers/post.js
@@ -1,41 +1,17 @@
 const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
-  res.json({
-    posts: [{ title: "JAI" }, { title: "SHREE" }, { title: "RAM" }],
-  });
+  const posts = Post.find()
+    .select("_id title body")
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => console.log(err));
 };
-
-// exports.createPost = (req, res) => {
-//   const post = new Post(req.body);
-//   // console.log("Creating Post : ", req.body);
-//   post.save((err, result) => {
-//     if (err) {
-//       return res.status();
-//     }
-//   });
-// };
-
-// exports.createPost = async (req, res) => {
-//   try {
-//     const post = new Post(req.body);
-//     // console.log("Creating Post : ", req.body);
-//     const result = await post.save();
-//     res.status(201).json(result);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// post.save().then((result) => {
-//   res.status(200).json({
-//     post: result,
-//   });
-// });
 
 exports.createPost = (req, res) => {
   const post = new Post(req.body);
   post.save().then((result) => {
-    res.status(200).json({ post: result });
+    res.json({ post: result }); //by default status is 200 for successful request. no need to mention it.
   });
 };
